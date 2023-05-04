5f6be6281441a695c9f129ba899898ffcbe5e5df
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2023-05-04 12:45:33 +0000

    commit
    Signed-off-by: Jyotirmoy Roy <jyotirmoyroy649@gmail.com>

--- components/Confirm.js
+++ components/Confirm.js
@@ -2,6 +2,7 @@ import RideSelector from "./RideSelector";
 import { useContext, useState } from "react";
 import { UberContext } from "../context/uberContext";
 import { ethers } from "ethers";
+import okayed from '../assets/okay.png'
 
 const style = {
   wrapper: `flex-1 h-full flex flex-col justify-between`,
@@ -9,7 +10,7 @@ const style = {
   confirmButtonContainer: ` cursor-pointer z-20 absolute bottom-[5px] bg-white left-0 right-0`,
   confirmButton: `bg-black text-white m-4 py-4 text-center text-xl`,
   popupContainer: `fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50`,
-  popup: `bg-white p-8 text-xl text-center`,
+  popup: `bg-white p-8 text-xl text-center justify-center`,
 };
 
 const Confirm = () => {
@@ -80,9 +81,29 @@ const Confirm = () => {
       </div>
       {transactionStatus === "success" && (
         <div className={style.popupContainer}>
-          <div className={style.popup}>Transaction successful!</div>
+          <div className={style.popup} style={{ flexDirection: "column" }}>
+            <img
+              src={okayed}
+              alt="Transaction successful"
+              className={style.successImage}
+              style={{ width: "200px" }}
+            />
+            <h1 style={{fontWeight: "bold"}}>Awesome!</h1>
+            <h4>Your order has been placed successfully</h4>
+            <button onClick={() => setTransactionStatus(null)} style={{
+              backgroundColor: "#7ED957",
+              color: "white",
+              width: "120px",
+              marginTop: "16px",
+              padding: "8px",
+              borderRadius: "4px",
+              cursor: "pointer",
+            }}
+            >OK</button>
+          </div>
         </div>
       )}
+
     </div>
   );
 };
